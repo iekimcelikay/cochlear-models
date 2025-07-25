715e327f1013af614003f4678adacb7222d5b975
ekim <i.ekimcelikay@gmail.com>
2025-07-25 13:35:03 +0200

    This is the version before I integrate the used functions into classes. NEed to test the memory and timing use.

--- subcorticalSTRF/BEZ2018_meanrate/run_ratemodel_with_matlabwrapper.py
+++ subcorticalSTRF/BEZ2018_meanrate/run_ratemodel_with_matlabwrapper.py
@@ -1,70 +1,31 @@
 # run_ratemodel_with_matlabwrapper.py
 # created: 27.06.2025 
-# last change: 27.06.2025
+# last change: 24.07.2025
 import sys
 import os
-from memory_profiler import memory_usage
 import psutil
-
 import time
-
 import matlab
-import numpy as np
-import pandas as pd
-from matlab_interface import setup_matlab_engine
+
 
 from datetime import datetime
 import gc
+from folder_manager import FolderManager
+from simulator_utils import _run_ihc_only_channel, generateANpopulation_separate_arrays
+from stimulus_utils import generate_tone_generator, calc_cfs, generate_stimuli_params
+from matlab_interface import setup_matlab_engine
+from soundgen import SoundGen
 
-start = time.time()
-from subcorticalSTRF.rf_simulator import (
-    # Stimulus utilities
-    generate_stimuli_params, generate_ramped_tone, generate_tone_dictionary, generate_tone_generator,
-    # Simulation utilities
-    calc_cfs, generateANpopulation, get_fiber_params, get_fiber_struct_array, generateANpopulation_separate_arrays
-)
-
-from subcorticalSTRF.rf_simulator.simulator_utils import _run_ihc_only_channel
-from subcorticalSTRF.stim_generator import SoundGen
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 ###----------------------------------------------------------------------------###
 def print_mem_usage():
 	process = psutil.Process(os.getpid())
 	mem = process.memory_info()
 	print(f"Memory usage: RSS = {mem.rss / (1024 **2):.2f} MB, VMS = {mem.vms / (1024 ** 2):.2f} MB")
-def compute_firing_rate_with_onset(psth_array, fs, psth_bin_width):
-	# Convert MATLAB double to NumPy array if needed
-	if not isinstance(psth_array, np.ndarray):
-		psth_array = np.array(psth_array).squeeze()
-	total_spikes = np.sum(psth_array)
-	duration_sec = len(psth_array) * psth_bin_width
-	return total_spikes / duration_sec
 
-def compute_firing_rate_no_onset(psth_array, fs, psth_bin_width, onset_skip_sec=0.01):
-	# Convert MATLAB double to NumPy array if needed
-	if not isinstance(psth_array, np.ndarray):
-		psth_array = np.array(psth_array).squeeze()
-	skip_bins = int(onset_skip_sec / psth_bin_width)
-	psth_no_onset = psth_array[skip_bins:]
-	duration_sec = len(psth_no_onset) * psth_bin_width
-	total_spikes = np.sum(psth_no_onset)
-	return total_spikes / duration_sec
-
-def compute_firing_rate_exclude_onset(psth_array, fs, stim_duration_sec, tau_ramp_sec = 0.01):
-	# Convert MATLAB double to NumPy array if needed
-	if not isinstance(psth_array, np.ndarray):
-		psth_array = np.array(psth_array).squeeze()
-	onset_sample = int(tau_ramp_sec * fs)
-	offset_sample = int(stim_duration_sec * fs)
-	stim_window = psth_array[onset_sample:offset_sample]
-	spike_count = stim_window.sum()
-	stim_window_duration = stim_duration_sec - tau_ramp_sec
-	return spike_count / stim_window_duration 
-
-def matlab_double_to_np_array(matlab_double_obj):
-	np_array = np.array(matlab_double_obj).squeeze()
-	return np_array
-
+def tone_generator_fn():
+	return generate_tone_generator(sound_gen, db_range, tone_freq_range, total_number_of_tone_freq, num_harmonics, tone_duration, harmonic_factor)
 
 def run_bruce2018_matlabwrapper(
     tone_generator_fn, #this is now a generator or any iterable yielding (db, freq, tone)
@@ -72,7 +33,9 @@ def run_bruce2018_matlabwrapper(
     peripheral_fs,
     num_ANF,
     eng,
-	run_idx=0
+	fibers_filename,
+	run_idx=0,
+	save_dir="." # Default to current directory
 ):
 	print("[INFO] Memory usage BEFORE running model:")
 	print_mem_usage()
@@ -82,11 +45,13 @@ def run_bruce2018_matlabwrapper(
 	dt = float(1 / peripheral_fs)
 	expliketype = float(1)  # softplus
 	implmnt = float(1)      # actual power-law implementation
-	noiseType = float(0)    # Random gaussian noise
+	noiseType = float(1)    # Random gaussian noise
 
 	all_synapse_results = []  # Python list to collect all results
-	all_synapse_params = [] # Python list to collect the relevant parameters 
+	all_synapse_params = [] # Python list to collect the relevant parameters
 
+	if not os.path.exists(save_dir):
+		os.makedirs(save_dir)
 
 	for cf_idx, cf in enumerate(cfs):
 		print(f"\n[INFO] Starting CF index {cf_idx} with CF = {cf:.1f} Hz")
@@ -115,36 +80,32 @@ def run_bruce2018_matlabwrapper(
 
 			# Run IHC
 			vihc = _run_ihc_only_channel(chan, eng)
-			print(f" cf is {cf} Hz ")	
-			print(f"cf index is {cf_idx}")
 
 			# --- Run MATLAB Synapse Model per CF-tone ---
 			try:
+				num_lsr, num_msr, num_hsr = num_ANF
+				print(f"[INFO] SYNAPSE tone_idx= {tone_idx}:freq = {freq} Hz @ {db} dB for CF= {cf:.1f} Hz")
+				print(f"[PARAMETERS] Number of ANF fibers: {num_lsr}, {num_msr}, {num_hsr}")
 				synapse_result_struct = eng.run_fiber_synapse_batch_parfor(
-					vihc,cf,cf_idx,freq, db, tone_idx, nrep, dt, noiseType, implmnt, expliketype, f"/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/ANpopulation_run{run_idx}.mat",nargout=1
+					vihc,cf,cf_idx,freq, db, tone_idx, nrep, dt, noiseType, implmnt, expliketype, fibers_filename, nargout=1
 				)
 			except matlab.engine.MatlabExecutionError as e:
 				print("MATLAB ERROR:\n", e)
 				raise
-			
 
-			# Optionally convert MATLAB struct to Python
-			all_synapse_results.append(synapse_result_struct)
+			# Save each synapse result immediately to disk
+			eng.workspace['synapse_result_struct'] = synapse_result_struct
+			filename =os.path.join(save_dir, f"synapse_result_cf{cf:.1f}hz_tone{freq:.1f}hz_{db}db_{num_lsr}-{num_msr}-{num_hsr}fibers_run{run_idx}.mat")
+			eng.save(filename, 'synapse_result_struct', nargout=0)
 
-	num_lsr, num_msr, num_hsr = num_ANF
-	# Optionally save in MATLAB workspace
-	eng.workspace['all_synapse_results'] = all_synapse_results
-	filename = f"synapse_results_2runs_run{run_idx}_{timestamp}_{num_lsr}-{num_msr}-{num_hsr}_RandomNoise.mat"
-	eng.save(filename, 'all_synapse_results', nargout=0)
-	print(f"[PARAMETERS] Number of ANF fibers: {num_lsr}, {num_msr}, {num_hsr}")
+			# Free memory
+		del synapse_result_struct, tone, vihc, chan
+		gc.collect()
 
-	return all_synapse_results
+	return
 
-	###===============================================================================================================
-# save results to 
 
-results_folder = "/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF"
-filename_fibers = os.path.join(results_folder, "ANpopulation.mat")
+###===============================================================================================================
 
 # Stimulus parameters
 sample_rate = 100e3
@@ -159,14 +120,17 @@ psth_bin_width = 0.0005
 
 # Inner ear model parameters
 peripheral_fs = 100e3
-num_cf = 3
+num_cf = 5
 freq_range = (125, 2500, num_cf)
-num_ANF = (32, 32, 32)  # LSR, MSR, HSR. this parameter is needed for the generateANpopulation.
+num_ANF = (64, 64, 64)  # LSR, MSR, HSR. this parameter is needed for the generateANpopulation.
+num_lsr, num_msr, num_hsr = num_ANF
 cfs = calc_cfs(freq_range, species='human')
-print(cfs)
 
-fibers_dict = generateANpopulation_separate_arrays(num_cf, num_ANF, filename=filename_fibers)
+model_folder = "/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/BEZ2018_meanrate"
+fibers_filename = os.path.join(model_folder, f"ANpopulation_{num_lsr}-{num_msr}-{num_hsr}.mat")
+fibers_dict = generateANpopulation_separate_arrays(num_cf, num_ANF, filename=fibers_filename)
 
+results_folder = "/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/BEZ2018_meanrate/results"
 # ================================
 # 2. STIMULI GENERATION
 # ================================
@@ -174,8 +138,6 @@ sound_gen = SoundGen(sample_rate, tau_ramp)
 desired_dbs, desired_freqs = generate_stimuli_params(
 	tone_freq_range, num_cf=total_number_of_tone_freq, db_range=db_range
 )
-def tone_generator_fn():
-	return generate_tone_generator(sound_gen, db_range, tone_freq_range, total_number_of_tone_freq, num_harmonics, tone_duration, harmonic_factor)
 
 # ================================
 # 3. START MATLAB ENGINE
@@ -183,25 +145,42 @@ def tone_generator_fn():
 eng = setup_matlab_engine('/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/BEZ2018_meanrate/BEZ2018a_model')
 eng.addpath('/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/BEZ2018_meanrate', nargout=0)
 eng.addpath('/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/BEZ2018_meanrate/results', nargout=0)
-
+# ✅ Start parallel pool with 2 workers if not already running
+eng.eval("if isempty(gcp('nocreate')); parpool('local', 8); end", nargout=0)
 
 
 # ================================
 # 4. RUN BRUCE2018 MODEL
 # ================================
+# Initialize FolderManager with parameters
+stimulus_parameters = {
+	"sample_rate": int(sample_rate),
+	"tau_ramp": float(tau_ramp),
+	"num_harmonics": int(num_harmonics),
+	"tone_duration": float(tone_duration),
+	"harmonic_factor": float(harmonic_factor),
+	"total_number_of_tone_freq": int(total_number_of_tone_freq),
+	"dbs": [float(db) for db in desired_dbs],
+	"freq_range": [float(freq) for freq in desired_freqs],
+	"number_of_cfs": int(num_cf),
+	"cfs": [float(cf) for cf in cfs],
+	"num_ANF": [int(num_lsr), int(num_msr), int(num_hsr)],
+}
+
 if __name__ == "__main__":
-	num_repeats = 16
+	num_repeats = 4
 	print("[INFO] Starting full batch simulation...")
 	start_time = time.time()
-	
+
+	# Initialize FolderManager once:
+	folder_manager = FolderManager(base_dir=results_folder, num_repeats=num_repeats, num_cf=num_cf, num_ANF=num_ANF, stimulus_params=stimulus_parameters)
+
+	# Create a folder for the entire batch, save stim params text
+	folder_manager.create_batch_folder()
+
 	for run_idx in range(num_repeats):
 		print(f"\n[INFO] Starting trial {run_idx + 1} of {num_repeats}")
-
-		filename = f"/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF/ANpopulation_run{run_idx}.mat"
-		fibers_dict = generateANpopulation_separate_arrays(num_cf, num_ANF, filename=filename)
-
-		eng.addpath('/home/ekim/PycharmProjects/subcorticalSTRF/subcorticalSTRF', nargout=0)
-		run_bruce2018_matlabwrapper(tone_generator_fn, cfs, peripheral_fs, num_ANF, eng, run_idx=run_idx)
+		run_bruce2018_matlabwrapper(tone_generator_fn, cfs, peripheral_fs, num_ANF, eng, fibers_filename, run_idx=run_idx, save_dir=folder_manager.results_folder)
 
 	end_time = time.time()
 	elapsed = end_time - start_time
