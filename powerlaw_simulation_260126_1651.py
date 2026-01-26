035da2dcbccafe0428fd6f06d027ce81dc11450f
ekim <i.ekimcelikay@gmail.com>
2026-01-26 17:00:15 +0100

    Add power law simulation script (draft for now), add the misc_functions to utils

---
+++ powerlaw_simulation_260126_1651.py
@@ -0,0 +1,33 @@
+# Created on: 26.01.2026, 16:51
+# In this script ======
+#   I will load the firing rates for each CF, which gives me the AN output.
+#   Then I will apply power function, - imitates the sharpening of tuning curves
+# after AN stages, in subcortical.
+# I want to find the n that gives me an optimum sharpening profile.
+# It might be interesting to compare this with a lateral inhibition operation
+# Idk though.
+# Checking different papers to identify the steps they apply after AN simulation
+# can be a good idea.
+# Created by: Ekim Celikay
+
+# 1. Apply power function
+# 2. Re-normalize (divide by pooled activity)
+
+# ================= Function drafts ====================
+
+def something_to_load_CF_firing_rates():
+    return
+
+def organize_for_eachtone_allCFs():
+    return
+
+def plot_spectrogram_forCFS():
+    return
+
+def power_function_sharpening():
+    return
+
+def plot_power_sharpened_curves():
+    return
+
+
